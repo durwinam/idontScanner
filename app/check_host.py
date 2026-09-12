0030f7ec3f4ac4cb97913044508d30a524f98eba
abol <aboliiideyliii@gmail.com>
2026-09-13 01:27:46 +0330

    fixed

--- app/check_host.py
+++ app/check_host.py
@@ -13,7 +13,7 @@ import urllib.request
 from typing import Any
 
 API_BASE = "https://check-host.net"
-USER_AGENT = "idontScanner-CheckHost/3.0.5"
+USER_AGENT = "idontScanner-CheckHost/3.0.6"
 MAX_NODES = 15
 IRAN_NODES = (
     "ir1.node.check-host.net",
